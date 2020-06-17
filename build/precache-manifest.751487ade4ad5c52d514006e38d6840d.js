79332d33415ce169cd997184b47b9e9c568d4925
Phazeshift <admin@phazeshift.co.uk>
2020-06-17 18:38:34 +0100

    Built react UI files

---
+++ build/precache-manifest.751487ade4ad5c52d514006e38d6840d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb45d7d5f0578cbbd732f75f86598ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5879a1e5b773be9bf1c7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df33e2bb6d16a78ad901",
+    "url": "/static/css/main.a2a34716.chunk.css"
+  },
+  {
+    "revision": "5879a1e5b773be9bf1c7",
+    "url": "/static/js/2.69210d8b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.69210d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df33e2bb6d16a78ad901",
+    "url": "/static/js/main.d1e1e743.chunk.js"
+  },
+  {
+    "revision": "a7d65176dce69fa2be31",
+    "url": "/static/js/runtime-main.248a35c9.js"
+  }
+]);
