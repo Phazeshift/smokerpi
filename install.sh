3406d4a85f2cb39b46b07f431f3cc7a31a5e9dfa
Phazeshift <admin@phazeshift.co.uk>
2020-07-11 14:49:04 +0100

    Updating install scripts

--- install.sh
+++ install.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 #
-# CraftBeer PI
+# SmokerPi
 #
 # type the following commands:
 # chmod +x install.sh
 # sudo ./install.sh
 # sudo reboot
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 clear
 cat << "EOF"
 
 ----------------------------------------------------------------------------
 
-   Weclome to Installation
-
-              (                                  (        
-              )\ )                 )             )\ )     
-             (()/(    )         ( /(    (   (   (()/( (   
-              /(_))  (      (   )\())  ))\  )(   /(_)))\  
-             (_))    )\  '  )\ ((_)\  /((_)(()\ (_)) ((_) 
-             / __| _((_))  ((_)| |(_)(_))   ((_)| _ \ (_) 
-             \__ \| '  \()/ _ \| / / / -_) | '_||  _/ | | 
-             |___/|_|_|_| \___/|_\_\ \___| |_|  |_|   |_| 
-                                             
+   Welcome to Installation
+  ____                  _             ____  _ 
+ / ___| _ __ ___   ___ | | _____ _ __|  _ \(_)
+ \___ \| '_ ` _ \ / _ \| |/ / _ \ '__| |_) | |
+  ___) | | | | | | (_) |   <  __/ |  |  __/| |
+ |____/|_| |_| |_|\___/|_|\_\___|_|  |_|   |_|
+                                                                                                                       
 ----------------------------------------------------------------------------
 
 EOF
 
-#Install pip (package installer):
-apt-get -y install python-setuptools
-easy_install pip
+chmod +x runserver.sh
 
-#Install Flask
-apt-get -y install python-dev
-apt-get -y install libpcre3-dev
+#Install pip & Flask
+apt-get -y install python-setuptools python-dev python3-pip
+
 cd api
-virtualenv venv
-source /venv/bin/activate
+#pip install virtualenv
+python3 -m venv venv
+source "./venv/bin/activate"
 pip install -r requirements.txt
+cd .. 
 
 while true; do
     read -p "Would you like to start SmokerPi automatically after boot? (y/n): " yn
